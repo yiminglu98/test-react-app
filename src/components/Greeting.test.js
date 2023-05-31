1fe2faa1c6ff7c77814ff839898640ea23417137
Yiming Lu <yiminglu2021@outlook.com>
2023-05-30 20:53:44 -0700

    lisence added and added extra test code

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 //my first test snapshot of codes
 
@@ -15,4 +16,31 @@ describe('Greeting components', () => {
         const helloThereElement = screen.getByText('Hello There', { exact: false });
         expect(helloThereElement).toBeInTheDocument();
     });
+
+    test('renders original welcome test when button not clicked', () => {
+        render(<Greeting />);
+
+        const originalElement = screen.getByText("It's nice to see you");
+        expect(originalElement).toBeInTheDocument();
+    });
+
+    test('renders "changed" when button clicked', () => {
+        render(<Greeting />);
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        const outputElement = screen.getByText("Change Text!");
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('dose not render "good to see you"  when button clicked', () => {
+        render(<Greeting />);
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        const outputElement =screen.queryByText('good to see you');
+        expect(outputElement).toBeNull();
+    } )
 });
